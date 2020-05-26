d3cfd09a1a50378982da94d867855a5870a1e469
scorpion140309 <scorpion140309@yahoo.co.jp>
2020-05-26 15:37:06 +0900

    問題の可変化が少し進んだ。

--- js/GMAPBF_TRAINING.js
+++ js/GMAPBF_TRAINING.js
@@ -15,28 +15,50 @@ class GMAPBF_TRAINING
 		return;
 	}
 
+	//--------------------------------------------------------------
+	//
+	//	private methods
+	//
+	//--------------------------------------------------------------
+
 	// GUIのIDを取得
 	GetElements_()
 	{
 		this.selector_category_ = document.getElementById('category');
 		this.selector_quiz_id_ = document.getElementById('quiz_id');
+
+		//
+		this.text_area_qestion_ = document.getElementById('question_id');
+	
+		//
+		this.text_area_ref_ = document.getElementById('reference_id');
+	
+		//
 		this.ary_btn_ans_ = new Array();
-		this.ary_btn_okng_ = new Array();
 		const ary_ans_id =
 		[
 			'ans0','ans1','ans2','ans3',
 		];
+		for (let i = 0; i < ary_ans_id.length; i++)
+		{
+			let elm = document.getElementById(ary_ans_id[i]);
+			this.ary_btn_ans_.push(elm);
+		}
+
+		//
+		this.ary_btn_okng_ = new Array();
 		const ary_okng_id =
 		[
 			'okng_0','okng_1','okng_2','okng_3',
 		];
-		for (let i = 0; i < 4; i++)
+		for (let i = 0; i < ary_okng_id.length; i++)
 		{
-			this.ary_btn_ans_.push(document.getElementById(ary_ans_id[i]));
-			this.ary_btn_okng_.push(document.getElementById(ary_okng_id[i]));
+			let elm = document.getElementById(ary_okng_id[i])
+			this.ary_btn_okng_.push(elm);
 		}
 		return;
 	}
+
 	// 科目のリストボックスにアイテムをセット
 	SetCategoryItems_()
 	{
@@ -80,13 +102,96 @@ class GMAPBF_TRAINING
 		return;
     }
     //
-    SetIndexes(a_category_id, a_quiz_id)
+    SetIndexes_(a_category_id, a_quiz_id)
     {
         this.selector_category_.selectedIndex = a_category_id;
-        this.selector_quiz_id_.selectedIndex = a_category_id;
+        this.selector_quiz_id_.selectedIndex = a_quiz_id;
         return;
+	}
+
+	//
+    GetQuestion_(a_category_id, a_quiz_id)
+    {
+		let ret_str_q = '企業が競争優位を保てるかどうかは企業の経営資源とやケイパビリティ次第であるという考え方を何と呼ぶか？';
+        return ret_str_q;
     }
 	
+	//
+    GetAnsers_(a_category_id, a_quiz_id)
+    {
+		let ret_ary_str_ans =
+		[
+			'リソース・ベースド・ビュー',
+			'アビリティ・ベースド・ビュー',
+			'ポテンシャル・ベースド・ビュー',
+			'コンピテンシー・ベースド・ビュー',
+		];
+		return ret_ary_str_ans;
+    }
+
+
+	////////////////////////////////////////////////////////////////
+	//
+	//	public methods
+	//
+	////////////////////////////////////////////////////////////////
+
+	//
+    ShowQuestion()
+    {
+		// 問題文
+		this.text_area_qestion_.value = this.GetQuestion_();
+
+		// 選択肢番号
+		for (let i = 0; i < this.ary_btn_okng_.length; i++)
+		{
+			let item_okng = this.ary_btn_okng_[i];
+			item_okng.style.backgroundColor = '#e0e0e0e0';
+			item_okng.value = i;
+			item_okng.style.backgroundImage = 'linear-gradient(0deg, #d0d0d0, #f0f0f0)';
+		}
+
+		// 選択肢
+		const ary_str_ans = this.GetAnsers_();
+
+		for (let i = 0; i < this.ary_btn_ans_.length; i++)
+		{
+			let item_a = this.ary_btn_ans_[i];
+			item_a.value = ary_str_ans[i];
+			item_a.style.backgroundImage = 'linear-gradient(0deg, #d0d0d0, #f0f0f0)';
+		}
+
+		//
+		this.text_area_ref_.value = '';
+
+        return;
+    }
+	//
+	ShowAnser_and_Reference(a_ans_id)
+	{
+		const correct_id = 0;
+
+		// 解答表示
+		for (let i = 0; i < this.ary_btn_okng_.length; i++)
+		{
+			let flag_okng = (i == correct_id);
+			let str_okng = '';
+			let col_okng = 'linear-gradient(0deg, #d0a0d0, #f0a0f0)';
+			if (flag_okng)
+			{
+				str_okng = '✓';
+				col_okng = 'linear-gradient(0deg, #a0e0a0, #a0f0a0)';
+			}
+			this.ary_btn_okng_[i].value = str_okng;
+			this.ary_btn_okng_[i].style.backgroundImage = col_okng;
+			this.ary_btn_ans_[i].style.backgroundImage = col_okng;
+		}
+	
+		//
+		this.text_area_ref_.value = '1-17 戦略形成に関する見方」 参照';
+		return;
+	}
+
 	//
 	func_dummy()
 	{
--- js/gmapbf_test.js
+++ js/gmapbf_test.js
@@ -1,102 +1,6 @@
 ﻿//
-function ShowQuestionSingle_(a_category_id, a_quiz_id)
-{
-	// 問題文
-	let item_q = document.getElementById('question_id');
-	item_q.value = '企業が競争優位を保てるかどうかは企業の経営資源とやケイパビリティ次第であるという考え方を何と呼ぶか？';
-
-	// 選択肢番号
-	const ary_okng_id =
-	[
-		'okng_0','okng_1','okng_2','okng_3',
-	];
-	for (let i = 0; i < ary_okng_id.length; i++)
-	{
-		let okng_id = ary_okng_id[i];
-		let item_okng = document.getElementById(okng_id);
-		item_okng.style.backgroundColor = '#e0e0e0e0';
-		item_okng.value = i;
-		item_okng.style.backgroundImage = 'linear-gradient(0deg, #d0d0d0, #f0f0f0)';
-	}
-
-	// 選択肢
-	const ary_ans_id =
-	[
-		'ans0','ans1','ans2','ans3',
-	];
-	const ary_str_ans =
-	[
-		'リソース・ベースド・ビュー',
-		'アビリティ・ベースド・ビュー',
-		'ポテンシャル・ベースド・ビュー',
-		'コンピテンシー・ベースド・ビュー',
-	];
-
-	for (let i = 0; i < ary_str_ans.length; i++)
-	{
-		let ans_id = ary_ans_id[i];
-		let item_a = document.getElementById(ans_id);
-		item_a.value = ary_str_ans[i];
-		item_a.style.backgroundImage = 'linear-gradient(0deg, #d0d0d0, #f0f0f0)';
-	}
-
-	let item_r = document.getElementById('reference_id');
-	item_r.value = '';
-
-	return;
-}
-
-function ShowAnser_and_Reference_(a_ans_id)
-{
-	const correct_id = 0;
-
-	// 解答表示
-	const ary_ans_id =
-	[
-		'ans0','ans1','ans2','ans3',
-	];
-	const ary_okng_id =
-	[
-		'okng_0','okng_1','okng_2','okng_3',
-	];
-	for (let i = 0; i < ary_okng_id.length; i++)
-	{
-		let okng_id = ary_okng_id[i];
-		let item_okng = document.getElementById(okng_id);
-		let flag_okng = (i == correct_id);
-		let str_okng = '';
-		let col_okng = 'linear-gradient(0deg, #d0a0d0, #f0a0f0)';
-		if (flag_okng)
-		{
-			str_okng = '✓';
-			col_okng = 'linear-gradient(0deg, #a0e0a0, #a0f0a0)';
-		}
-		item_okng.value = str_okng;
-		item_okng.style.backgroundImage = col_okng;
-		document.getElementById(ary_ans_id[i]).style.backgroundImage = col_okng;
-	}
-
-	let item_r = document.getElementById('reference_id');
-	item_r.value = '1-17 戦略形成に関する見方」 参照';
-	return;
-}
-
+// GMAPBF_TRAINING のインスタンスを生成し、何らかの処理を実行させる。
 //
-function SetCategory_()
-{
-	let select = document.getElementById('category')
-	select.options.length = 0;
-
-	const ary_category = ['経営戦略','マーケティング','アカウンティング','人・組織']
-	for (let i = 0; i < ary_category.length; i++)
-	{
-		select.options[i] = new Option(ary_category[i]);
-	}//
-	//
-	select.options.selectedIndex = 0;
-
-	return;
-}
 
 //
 var gt = null;
@@ -105,23 +9,28 @@ var gt = null;
 function Init()
 {
 	gt = new GMAPBF_TRAINING();
-	gt.func_dummy();
-	//SetCategory_();
-	ShowQuestionSingle_(0, 0);
+	gt.ShowQuestion();
 	return;
 }
 
 //
 function ExeAll()
 {
-	ShowQuestionSingle_(0, 0);
+	if (gt != null)
+	{
+		gt.ShowQuestion();
+	}
 	return;
 }
 
 //
 function Anser(a_ans_id)
 {
-	ShowAnser_and_Reference_(a_ans_id);
+	//ShowAnser_and_Reference_(a_ans_id);
+	if (gt != null)
+	{
+		gt.ShowAnser_and_Reference(a_ans_id);
+	}
 	return;
 }
 
